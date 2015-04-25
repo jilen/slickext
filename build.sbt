11c867003ec54b49fce447f2f6e33a438cc9ef25
djx314 <djx314@sina.cn>
2015-04-25 23:36:18 +0800

    fix case class 22 columns problem and add some tests.

--- build.sbt
+++ build.sbt
@@ -8,8 +8,9 @@ resolvers += Resolver.sonatypeRepo("releases")
 
 
 libraryDependencies ++= Seq(
-  "org.scala-lang"     % "scala-compiler" % scalaVersion.value,
-  "com.typesafe.slick" %% "slick"         % "2.1.0"
+  "org.scala-lang" % "scala-compiler" % scalaVersion.value,
+  "com.typesafe.slick" %% "slick" % "2.1.0",
+  "com.h2database" % "h2" % "1.4.187"
 )
 
 addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0-M5" cross CrossVersion.full)
--- src/test/scala/slick/ext/repo.scala
+++ src/test/scala/slick/ext/repo.scala
@@ -3,9 +3,34 @@ package slick.ext
 import slick.ext.macros._
 
 trait UserRepo {
-  val profile: scala.slick.driver.JdbcProfile
+  //val profile: scala.slick.driver.JdbcProfile
+  val profile = scala.slick.driver.H2Driver
   import profile.simple._
+  import scala.slick.collection.heterogenous._
+  
+  val db = Database.forURL("jdbc:h2:mem:slickextTest", driver = "org.h2.Driver")
 
   @table[User](tableName = "user")
   class Users
+  
+  //I suggest that TableQuery's name use model + "Table" to avoid the table name like "news"
+  @table[TestUser](tableName = "test_user")
+  class TestUserTable
+  
+}
+
+object TestCase extends App {
+  
+  object UserRepo extends UserRepo
+  
+  
+  import UserRepo._
+  import UserRepo.profile.simple._
+  db withSession { implicit session =>
+    Users.ddl.create
+    TestUserTable.ddl.create
+    TestUserTable ++= TestUser(None, Option(2333), "东方") :: TestUser(None, Option(6666), "在地下城寻求偶遇是否搞错了什么") :: TestUser(None, Option(9527), "刀剑神域") :: Nil
+    println(TestUserTable.list.mkString("\n\n"))
+  }
+  
 }
